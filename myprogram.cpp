5d43c6187eefd3116ec679f53bb3e6fb00dc462b
Stanislav Goria4ev <stas.goria4ev@mail.ru>
2022-12-06 11:15:10 +0400

    correction of remarks

--- myprogram.cpp
+++ myprogram.cpp
@@ -3,83 +3,93 @@
 #include <thread>
 #include <cmath>
 #include <vector>
-
-void Move(int speed, int* meters) { 
-    std::this_thread::sleep_for(std::chrono::seconds(0)); //нить реализации движения пловца
-    *meters += speed;
-}
+//#include <mutex>
 
 class Swimmer {
 public:
     std::string name = "unknown";
     int speed = 0;
     int meters = 0;
-    bool finish = false;
     int timeSec = 0;
 };
 
+std::vector <Swimmer> swimmers;
+//std::mutex swimmers_access;
+
+void Move() { 
+    for (int i = 0; i < swimmers.size(); ++i) {
+        //swimmers_access.lock();
+        swimmers[i].meters += swimmers[i].speed;
+        //swimmers_access.unlock();
+    }
+}
+
 class Swim {
 public:
-    std::vector <Swimmer> swimmers;
-    std::vector <Swimmer> results;
     int timeSec = 0;
-
-    bool SpeedComparison(int& speed) {
-        bool found = false;
-        for (int i = 0; i < swimmers.size(); ++i) {
-            if (swimmers[i].speed == speed) 
-                return true;
-        }
-        return found;
-    }
     
     void Display() {
         std::cout << "Start!\n\n";
-        while (!swimmers.empty()) {
+        bool found = true;
+        while (found) {
             timeSec++;
+            //std::this_thread::sleep_for(std::chrono::seconds(1));
+            
+            std::thread moveSwimmer(Move);
+
             std::this_thread::sleep_for(std::chrono::seconds(1)); //нить задержки 1 сек для Вывода движений пловцов
+            int count = 0;
             for (int i = 0; i < swimmers.size(); ++i) {
-                swimmers[i].timeSec = timeSec;
-                std::thread moveSwimmer(Move, swimmers[i].speed, &swimmers[i].meters);
-                moveSwimmer.join();            
-                if (!swimmers[i].finish && swimmers[i].meters >= 100) {
-                    swimmers[i].finish = true;
-
-                    results.push_back(swimmers[i]);
-
+                //swimmers_access.lock();
+                if (swimmers[i].timeSec == 0 && swimmers[i].meters >= 100) {
+                    swimmers[i].timeSec = timeSec;
                     std::cout << swimmers[i].name << " Finish - 100 meters!" << '\n'; 
-                } else if (!swimmers[i].finish) 
+                } else if (swimmers[i].timeSec != 0) 
+                    count++;
+                else
                     std::cout << swimmers[i].name << " swam " << swimmers[i].meters << " meters\n";
+                
+                //swimmers_access.unlock();
             }
+
+            moveSwimmer.join();
+
             std::cout << '\n'; 
 
-            for (int s = 0; s < swimmers.size(); ++s) {                              
-                for (int q = 0; q < swimmers.size(); ++q) {                                
-                    if (swimmers[q].finish) 
-                        swimmers.erase(swimmers.begin() + q);
-                } 
-            }
+            if (count == swimmers.size())
+                found = false;
         } 
+    }
+
+    void Bubble() const {
+        for (int s = 0; s < swimmers.size(); ++s) { 
+            for (int q = 0; q < swimmers.size() - 1; ++q) {                                
+                if (swimmers[q].timeSec > swimmers[q + 1].timeSec) {
+                    Swimmer temp[1];
+                    *temp = swimmers[q];
+                    swimmers[q] = swimmers[q + 1];
+                    swimmers[q + 1] = temp[0];
+                }
+            } 
+        }
     }
 };
 
 int main() { 
     Swim swim;
-    for (int i = 0; i < 6; ++i) {
+    for (int i = 0; i < 6; ++i) { 
         Swimmer swimmer;
-        bool good;
         std::cout << i + 1 << ". Enter swimmer's name: "; std::cin >> swimmer.name;
-        do {
-            std::cout << "And his speed: "; std::cin >> swimmer.speed;
-            good = swim.SpeedComparison(swimmer.speed);
-            if (good) std::cout << "Repetitive speed, please enter another value\n";
-        } while (good);
-        swim.swimmers.push_back(swimmer);
+        std::cout << "And his speed: "; std::cin >> swimmer.speed;
+        swimmers.push_back(swimmer);
     }
     std::cout << '\n';
     
     swim.Display();
     
-    for (std::vector <Swimmer>::iterator it = swim.results.begin(); it != swim.results.end(); ++it)
+    swim.Bubble();
+    
+    
+    for (std::vector <Swimmer>::iterator it = swimmers.begin(); it != swimmers.end(); ++it)
         std::cout << "time = " << it->timeSec << ", swimmer = " << it->name << '\n';
 }
